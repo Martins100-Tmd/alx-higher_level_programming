261e7d29ab96f5c2a43792f5e82a139f384f511c
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-22 02:21:14 -0700

    PYTHON

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+l=str*3;
+c=str[:9];
+print("%s\n%s\n"%(l,c));
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str=str[str.index("object"):str.index("language")]+"with python";
+print(str);
+~
