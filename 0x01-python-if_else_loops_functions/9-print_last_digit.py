6808a629ee0f321e35388d36034924a1aa749f03
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-23 12:46:00 -0700

    Python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    temp = int(repr(number)[-1]);
-    print("{}".format(temp),end='');
-    return temp;
+    temp = int(repr(number)[-1])
+    print("{}".format(temp), end='')
+    return temp
