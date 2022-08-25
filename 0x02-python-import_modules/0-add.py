d758f9cd190e75a70bbcd8d197d6d85452612b4e
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-25 10:33:12 -0700

    Python import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0.py as MATH
-
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-print("{} + {} = {}".format(a, b, int(MATH.add(a,b))))
+print("{} + {} = {}".format(a, b, (add(a,b))))
+
