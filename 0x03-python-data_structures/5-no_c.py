d230a0b471acf736b58dab03bc6fcd2b69c7c384
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-28 01:12:57 -0700

    Data Structures -> Python

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    strList=list()
+    sr=''
+    for i in range(len(my_string)):
+        strList.append(my_string[i])
+    if 'C' in strList:
+        strList.remove('C')
+    elif 'c' in strList:
+        strList.remove('c')
+    for i in range(len(strList)):
+        sr+=strList[i]
+    return sr
