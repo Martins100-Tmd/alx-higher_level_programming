dc25ce240dc86ac4baa5a1568cc7eea910aa55ef
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-29 13:03:44 -0700

    Data structures -> Python

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,52 +1,51 @@
 #include "lists.h"
 /**
+* list_len - finds no. of elements ina linked list.
+* @h: pointer to linked list.
+*
+* Return: number of elements in linked list.
+*/
+size_t list_len(listint_t *h)
+{
+	size_t  nodes = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
+/**
  * is_palindrome - function to check for a palindrome linked list
  * @head: pointer to a pointer pointing to a struct
  * Return: integer
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *help = *head, *some = *head, *rome = *head;
-    int i = 0, countcopy;
-    unsigned int count = 0;
-    while (help != NULL)
-    {
-        count++;
-        help = help->next;
-    }
-    countcopy = count;
-    int *arr = (int *)malloc(sizeof(countcopy * sizeof(int)));
-    while (some != NULL)
-    {
-        *(arr + i) = some->n;
-        some = some->next;
-        i++;
-    }
-    int *brr = (int *)malloc(sizeof(countcopy * sizeof(int))), k = countcopy - 1, K = 0;
-    for (; k >= 0; k--)
-    {
-        *(brr + K) = *(arr + k);
-        K++;
-    }
-    int *crr = (int *)malloc(sizeof(countcopy * sizeof(int))), L = 0, result = 0;
-    for (; L < countcopy; L++)
-    {
-        if (brr[L] == arr[L])
-        {
-            crr[L] = 1;
-            result++;
-        }
-        else
-        {
-            crr[L] = 0;
-        }
-    }
-    if (result == countcopy)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	int *nArr, i = 0, j = 0, len = 0;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (1);
+	temp = *head;
+	len = list_len(temp);
+	nArr = (int *)malloc(sizeof(int) * len);
+	if (nArr == NULL)
+		return (2);
+	temp = *head;
+	while (temp != NULL)
+	{
+		nArr[j] = temp->n;
+		j++;
+		temp = temp->next;
+	}
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		if (nArr[i] != nArr[j])
+			return (0);
+	}
+	return (1);
 }
