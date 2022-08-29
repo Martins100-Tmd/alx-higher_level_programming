5d84f2ff2b5d11b256fa1d744bc76fba69701cf2
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-29 11:04:41 -0700

    Data structures -> Python3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ def no_c(my_string):
         strList.append(my_string[i])
     for i in range(len(strList)):
         if strList[i] != 'C' and strList[i] != 'c':
-            sr += strList[i];
+            sr += strList[i]
     return sr
