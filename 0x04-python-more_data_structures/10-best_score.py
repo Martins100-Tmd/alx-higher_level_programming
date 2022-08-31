93d26388ce819f87f56badde38539288ae33ba9a
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-31 06:07:00 -0700

    Data structures -> Python3

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    arr = list()
-    store = dict()
-    for k, v in a_dictionary.items():
-        arr.append(v)
-        store.update({v:k})
-    arr.sort()
-    return store[arr[-1]]
+    if a_dictionary:
+        arr = list()
+        store = dict()
+        for k, v in a_dictionary.items():
+            arr.append(v)
+            store.update({v:k})
+        arr.sort()
+        return store[arr[-1]]
+    else:
+        return None
