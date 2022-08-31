fae862c70af317c81f4c614f3a0c17731f69c4fd
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-31 07:05:16 -0700

    Data structures -> Python

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
-        ROMAN_MAP = {
-            'M': 1000,
-            'D': 500,
-            'C': 100,
-            'L': 50,
-            'X': 10,
-            'V': 5,
-            'I': 1
-        }
-        total = 0
-        prev = 0
-        for i in range(len(roman_string)):
-            curr = ROMAN_MAP[roman_string[i]]
-            if curr > prev:
-                total += curr - 2 * prev
-            else:
-                total += curr
-            prev = curr
-        return total
+    if not isinstance(roman_string, str):
+        return 0
+    total = 0
+    num = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for r in reversed(roman_string):
+        num = digits[r]
+        total += num if total < num * 5 else -num
+    return total
