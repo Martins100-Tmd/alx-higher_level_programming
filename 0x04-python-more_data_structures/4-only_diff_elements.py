cfa1d95f90057e7e47cfa644db2d2b6bacb91c2c
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-08-31 05:08:05 -0700

    Data structures -> Python

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    L1 = list(set_1)
-    L2 = list(set_2)
-    for i in L1:
-        L2.append(i)
-    L2.sort()
-    res = set(L2)
-    return res
+    return set_1 ^ set_2
