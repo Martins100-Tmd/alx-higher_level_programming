35a8ae6c93eeafa55b5dd39f0938138b2d85817a
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-26 04:07:58 -0700

    Class Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Checks for instance of an object."""
+
+def is_kind_of_class(obj, a_class):
+    """return True if obj is an instance, and False if it is not."""
+    return isinstance(obj, a_class)
