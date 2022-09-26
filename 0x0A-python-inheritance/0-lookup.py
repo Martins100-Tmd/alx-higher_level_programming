47b1681994e33d7d7f1cfcf4ea39f742f6c735a6
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-26 03:27:09 -0700

    Classes Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Makes a function that return all availaible atrributes, 
+and methods.
+"""
+
+def lookup(obj):
+    return dir(obj)
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Same class"""
+
+def is_same_class(obj, a_class):
+    """check for the instance of an object"""
+    return isinstance(obj, a_class);
