c3a535e4898bc5dcf16338ef7a3b28a142b74642
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-26 07:36:07 -0700

    Class inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define int class"""
+
+
+class MyInt(int):
+    """MyInt class"""
+    def __eq__(self, value):
+        """Get invoked when == is called on the instance and value"""
+        return self.real != value
+    def __ne__(self, value):
+        """get invoked when != is called on the instance and the value"""
+        return self.real == value
