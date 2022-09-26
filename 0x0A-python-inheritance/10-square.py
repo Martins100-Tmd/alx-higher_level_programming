e30ddb9e6ed581dd3c1fd4fa6ef74376cd6e69fa
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-26 07:08:08 -0700

    Class inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square rectangle"""
+    def __init__(self, size):
+        """Create own size prop"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def Area(self):
+        super().Area(self.__width, self.__height)
