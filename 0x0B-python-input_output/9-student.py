b135e5cbf62441ad8b596dbcf2450c42f4272830
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-27 10:33:20 -0700

    File I/O

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,13 +5,19 @@
 class Student:
     """Student class created"""
 
+
     first_name = ""
     last_name = ""
     age = 0
 
     def __init__(self, first_name, last_name, age):
-        """Instance of student will be initialised"""
+        """Instance of student will be initialised
 
+        Args:
+            first_name(string): new instance first_name
+            last_name(string): new instance last_name
+            age(number): new instance age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
