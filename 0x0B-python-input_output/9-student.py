c63f1eae07bdc23dfba30e98c028d7e6c9315cc5
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-27 10:34:45 -0700

    File I/O

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""Define a student class"""
+''' a class Student that defines a student
+'''
 
 
 class Student:
-    """Student class created"""
-
-
-    first_name = ""
-    last_name = ""
-    age = 0
+    '''module class student
+    '''
 
     def __init__(self, first_name, last_name, age):
-        """Instance of student will be initialised
-
-        Args:
-            first_name(string): new instance first_name
-            last_name(string): new instance last_name
-            age(number): new instance age
-        """
+        '''method __init__
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """return __dict__ of the new class instance"""
-        return {'first_name': self.first_name, 'last_name': self.last_name, 'age': self.age}
+        '''method to_json
+        '''
+        return self.__dict__
