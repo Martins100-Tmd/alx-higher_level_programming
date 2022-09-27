5bea411f5aadb1947c9757a30a715422e52483dd
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-27 11:01:34 -0700

    File I/O

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' a class Student that defines a student
+'''
+
+
+class Student:
+    '''module class student
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''method __init__
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''method to_json
+        '''
+        return self.__dict__
+    def reload_from_json(self, json):
+        '''
+        method to setattr to replace all attr of student instance
+
+        Args:
+            json(dict): student instance dictionary
+        '''
+        for k, v in json.items():
+            setattr(self, k, v)
