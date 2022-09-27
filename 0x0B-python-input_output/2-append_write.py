b9efb9341614cd6efda1b9c806aa2b7e1b4501ef
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-27 08:42:29 -0700

    File I/O

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,11 +12,4 @@ def append_write(filename="", text=""):
         No errors
     """
     with open(filename, 'r+', encoding='utf-8') as f:
-        f.read()
-        len1 = f.tell()
-        f.write(text)
-        len2 = f.tell()
-        len = len1 - len2
-        if len < 0:
-            return -1 * len
-        return len
+        return f.write(text)
