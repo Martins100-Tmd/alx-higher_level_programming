a7d5c7bf4962492bcc3984eeb4d8845cb118258e
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-10-03 10:41:39 -0700

    Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a Base class"""
+
+
+
+class Base:
+    """Base class created"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Base class init
+
+        Args:
+            id(any): new instance id
+        Errors:
+            No Error
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
