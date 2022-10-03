9a036cdd41713d8580fdb0fa0d4b9dc2e863e243
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-10-03 12:11:58 -0700

    Almost a cirlce

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define a Base class"""
+import json
 
 
 class Base:
@@ -20,3 +21,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """return JSON string"""
+        if list_dictionaries == None:
+            return "[]"
+        return json.dumps(list_dictionaries)
