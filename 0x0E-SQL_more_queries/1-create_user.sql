c9510d0851181a40b6ebd04abc0617fca367af20
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-11-02 13:11:34 -0700

    More SQL Queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
 -- Creates a user and grant it all privileges
 
-mysql --user='user_0d_1'
-GRANT ALL PRIVILEGES TO 'user_0d_1'@'localhost'
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'localhost';
