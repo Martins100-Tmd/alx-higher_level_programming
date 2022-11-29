bb79949cd7a58d08442e9c4921ea5acc048e5159
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-11-29 11:56:09 -0800

    JS

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const process = require('process');
+const num = Math.floor(parseInt(process.argv[2]));
+
+if (process.argv[2]) {
+  if (num > 0) {
+    if (typeof num === 'number') {
+      let sum = '';
+      let i = 0;
+      while (i < num) {
+        sum += 'X';
+        i++;
+      }
+      console.log(sum);
+    }
+  }
+} else {
+  console.log('Missing size');
+}
