d2dd519e7b910aceb1c70238a37095a6d643b422
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-12-06 11:11:55 -0800

    Object-relational Mapper

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+import MYSQLdb
+from sys import argv
+
+
+'''
+a script that list all the list of row
+in a database
+'''
+if __name__ == "__main__":
+    conn = MYSQLdb.connect(
+         host="localhost", port=3306, user=argv[1],
+         passwd=argv[2], db=argv[3], charset="utf-8")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    table_rows = cur.fetchall()
+    for row in table_rows:
+        print(row)
+    cur.close()
+    conn.close()
