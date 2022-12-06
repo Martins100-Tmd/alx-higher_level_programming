839a696f5c8b46d3202fe431f336c0b22ec37dbc
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-12-06 11:19:13 -0800

    Object-relational Mapper

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,6 @@
 import MYSQLdb
 from sys import argv
 
-
 '''
 a script that list all the list of row
 in a database
@@ -14,10 +13,9 @@ if __name__ == "__main__":
          host="localhost", port=3306, user=argv[1],
          passwd=argv[2], db=argv[3], charset="utf-8")
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     table_rows = cur.fetchall()
     for row in table_rows:
         print(row)
     cur.close()
-    conn.close()
+    table_rows.close()
