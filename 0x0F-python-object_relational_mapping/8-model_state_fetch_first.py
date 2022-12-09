5992853503cc16bfb8442f36176fac005ed9b4df
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-12-09 07:52:42 -0800

    ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,7 +4,6 @@
 prints the first state object from the database
 '''
 
-
 from sqlalchemy import create_engine
 from sys import argv
 from model_state import Base, State
@@ -22,5 +21,4 @@ if __name__ == "__main__":
     qry = engine.execute("SELECT * from states WHERE id=1")
     db = qry.fetchall()
     db = db[0]
-
     print("{}: {}".format(db[0], db[1]))
