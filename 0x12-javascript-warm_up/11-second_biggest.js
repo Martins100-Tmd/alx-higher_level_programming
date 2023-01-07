f2762d9de580f87082cd6efc9260ad2527fc43c1
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-07 07:05:03 -0800

    JavaScript

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// write a script that search for the
+// second largest integer in a list.
+const process = require('process');
+let array = process.argv.slice(2).map((item) => {
+  return parseInt(item);
+});
+if (process.argv.length === 2) {
+  console.log(0);
+} else {
+  array = array.sort((a, b) => {
+    return a - b;
+  });
+  console.log(array[array.length - 2]);
+}
