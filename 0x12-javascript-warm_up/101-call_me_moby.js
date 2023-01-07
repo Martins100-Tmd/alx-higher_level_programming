40f64dfcca9c05cc49546a7df4cf8df63a379e9b
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-07 07:34:43 -0800

    JavaScript

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-//write a script that calls a function x times
+// write a script that calls a function x times
 
-exports.callMeMoby = function(x, func){
-	while(x !== 0)
-	{
-		func();
-		--x;
-	}
-}
-
+exports.callMeMoby = function (x, func) {
+  while (x !== 0) {
+    func();
+    --x;
+  }
+};
