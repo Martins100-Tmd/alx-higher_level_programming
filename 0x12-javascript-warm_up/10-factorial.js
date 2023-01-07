cb274d8af0f50a087527d117b1907fd69eecf7f5
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-07 06:40:20 -0800

    JavaScript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// write a script that computes and prints a factorial.
+const process = require('process');
+const num = Math.floor(parseInt(process.argv[2]));
+function RecurseFac (b, start = 1) {
+  if (b === 1) {
+    console.log(start * num);
+    return 1;
+  }
+  --b;
+  start *= b;
+  return RecurseFac(b, start);
+}
+RecurseFac(num);
