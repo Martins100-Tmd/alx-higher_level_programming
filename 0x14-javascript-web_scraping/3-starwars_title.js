921c7568c75edae259328fd758574e160ed43357
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-17 06:26:38 -0800

    commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// print movie where episode number matches integer
+const request = require('request');
+const id = require('process').argv[2];
+request('https://swapi-api.alx-tools.com/api/films/' + id, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
