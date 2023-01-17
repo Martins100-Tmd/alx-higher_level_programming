884255a5ea3052232d6c1be0828664282cbdb308
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-17 05:50:35 -0800

    commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-//Write a script that reads and prints the content of a file
-const fs = require('fs')
-const process = require('process')
-const file = process.argv[2]
-fs.readFile(file, 'utf8', (err, data)=>{
-	if(err)console.log(err)
-	console.log(data);
-})
+// Write a script that reads and prints the content of a file
+const fs = require('fs');
+const process = require('process');
+const file = process.argv[2];
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err)console.log(err);
+  console.log(data);
+});
