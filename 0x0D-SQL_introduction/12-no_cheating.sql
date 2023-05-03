81d1cdb2777e9f792f0443b98f4dbbd684c104d0
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-05-03 05:21:50 -0700

    SQL

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,2 @@
--- updates the score of Bob to 10
-
-UPDATE second_table
-SET score = 10
-WHERE name = 'Bob'
+-- change bob's score to 10 from 14
+UPDATE second_table SET score = 10 WHERE name="Bob";
