de19c5acdc7405b439717e2c886745b40ba9630f
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-05-03 05:14:13 -0700

    SQL

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
--- list all records with scoe >= 10
-
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC 
+-- list all row with a score of 10 or more
+SELECT score, name FROM second_table WHERE score >= 10;
