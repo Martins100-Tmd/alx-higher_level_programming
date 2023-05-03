c7102ed9e6e76d616cf0f786865081ac85402934
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-05-03 05:07:14 -0700

    SQL

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,2 @@
--- list all records of the table second_table
-
-SELECT score, name FROM second_table ORDER BY score DESC
+-- list all records in a table of a database
+SELECT second_table.score, second_table.name FROM second_table ORDER BY second_table.score DESC
