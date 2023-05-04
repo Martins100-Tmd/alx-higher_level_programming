fb1fe210f9921005c9059b27369ace3bc016e60a
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-05-04 07:13:58 -0700

    More Queries

--- 0x0E-SQL_more_queries/3m.sql
+++ 0x0E-SQL_more_queries/3m.sql
@@ -1,6 +1,5 @@
--- creates user and database
+-- creates a table force_name
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+CREATE TABLE IF NOT EXISTS 'force_name'(
+	'id' INT,
+        'name' VARCHAR(256) NOT NULL);
